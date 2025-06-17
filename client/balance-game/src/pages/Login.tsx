934cec653a29043805a3f3b8aa7ecbaa72903994
MinSung Kwon <csstumings804@gmail.com>
2025-06-17 15:54:10 +0900

    로그인 기능 구현

--- client/balance-game/src/pages/Login.tsx
+++ client/balance-game/src/pages/Login.tsx
@@ -1,9 +1,36 @@
-import React from 'react'
+import axios from 'axios';
+import React, { useState, type ReactEventHandler } from 'react'
 import { useNavigate } from 'react-router-dom'
 export const Login = () => {
+  const [id, setID] = useState('');
+  const [pw, setPW] = useState('');
+  const [errmsg, setErrmsg] = useState<string | null>(null);
+
   const navigate = useNavigate();
   const toSignUp = () => {
     navigate('/signup')
+  }
+  const handleIDChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setID(e.target.value);
+  }
+  const handlePWChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setPW(e.target.value);
+  }
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+    try {
+      const response = await axios.post('http://localhost:5050/api/login', {
+        login_id: id, 
+        password: pw,
+      })
+      const token = response.data.token;
+      localStorage.setItem('token', token);
+      navigate('/')
+    } catch (error) {
+      setID('')
+      setPW('')
+      setErrmsg("다시 입력해주세요.")
+    }
   }
   return (
     <div className="min-h-screen bg-gray-100 flex flex-col">
@@ -17,6 +44,8 @@ export const Login = () => {
             <label className="block text-sm font-medium mb-2">ID</label>
             <input
               type="text"
+              onChange={handleIDChange}
+              value={id}
               placeholder="아이디 입력하세요"
               className="w-full border border-gray-400 p-3 rounded focus:outline-none focus:ring-2 focus:ring-blue-400"
             />
@@ -27,6 +56,8 @@ export const Login = () => {
             <label className="block text-sm font-medium mb-2">PASSWORD</label>
             <input
               type="password"
+              onChange={handlePWChange}
+              value={pw}
               placeholder="비밀번호 입력하세요"
               className="w-full border border-gray-400 p-3 rounded focus:outline-none focus:ring-2 focus:ring-blue-400"
             />
@@ -34,10 +65,13 @@ export const Login = () => {
 
 
           {/* 로그인 버튼 */}
-          <button className="w-full bg-blue-500 text-white py-3 rounded hover:bg-blue-600 transition">
+          <button 
+          onClick={handleSubmit}
+          className="w-full bg-blue-500 text-white py-3 rounded hover:bg-blue-600 transition">
             로그인
           </button>
           <div className='text-xs text-red-400 mt-2'>회원이 아니신가요?<span className='underline cursor-pointer' onClick={toSignUp}>회원가입 하러가기</span></div>
+          {errmsg && <div className='text-xs text-red-400 mt-2'>{errmsg}</div>}
         </form>
       </div>
     </div>
